f7663a21cab9f2de6a58739bed5f7f51d902f25d
shadab4476 <shadabansari7726@gmail.com>
2024-08-10 18:50:16 +0530

    backend store trade complete

---
+++ app/Http/Controllers/TradeController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trade;
+use Illuminate\Http\Request;
+
+class TradeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $trade = Trade::get();
+
+        // if ($trade->count()) {
+        return response()->josn([
+            "status" => true,
+            "data" => $trade->count(),
+        ]);
+        // }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $validator = Validator()->make($request->all(), [
+            'stock_name' => 'required',
+            'owner_name' => 'required',
+            'price' => 'required|numeric|min:1',
+            'quantity' => 'required|numeric|min:1',
+            'date' => 'required|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => false,
+                "errors" => $validator->errors()
+            ]);
+        }
+        $trade =  Trade::create([
+            "stock_name" => $request->stock_name,
+            "owner_name" => $request->owner_name,
+            "price" => $request->price,
+            "quantity" => $request->quantity,
+            "date" => $request->date,
+        ]);
+        return response()->json([
+            "status" => true,
+            "data" => $trade,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_10_063958_create_trade_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->id();
+            $table->string("stoke_name");
+            $table->string("owner_name");
+            $table->string("price");
+            $table->integer("quantity");
+            $table->date("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trades');
+    }
+};
